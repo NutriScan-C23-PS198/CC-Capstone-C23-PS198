ff88d13eefbbbbf6c3433bc7b3921f06d0d1a9c4
risydah13 <125931636+risydah13@users.noreply.github.com>
2023-05-30 17:07:12 +0700

    Add files via upload

---
+++ src/routes/users.js
@@ -0,0 +1,12 @@
+module.exports = function usersRouter(express, verifyToken, usersController) {
+    const router = express.Router();
+  
+    router.get('/', usersController.getAllUsers);
+    router.get('/:id', usersController.getUserById);
+    // router.get('/current', usersController.getCurrentUser);
+    router.post('/register', usersController.createUser);
+    router.post('/login', usersController.login);
+    router.delete('/:id', usersController.deleteUserById);
+  
+    return router;
+  };
