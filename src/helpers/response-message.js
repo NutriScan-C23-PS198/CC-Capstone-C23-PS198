11be77487f6d5fb3a99fbf318a45f033bbb6c469
risydah13 <125931636+risydah13@users.noreply.github.com>
2023-05-30 17:03:55 +0700

    Add files via upload

---
+++ src/helpers/response-message.js
@@ -0,0 +1,20 @@
+module.exports = {
+    users: {
+      create: 'user has been created',
+      notFound: 'user not found',
+      delete: 'user has been deleted',
+      emailExist: 'email already exists',
+      loginSuccess: 'login successfully',
+      invalidCredential: 'your password or email wrong',
+      tokenRequired: 'token required',
+      invalidToken: 'token is invalid',
+      notLogin: 'user not login',
+      invalidEmail: 'invalid email',
+      minimumPass: 'password must be at least 6 characters',
+    },
+    food: {
+      create: 'food has been created',
+      delete: 'food has been deleted',
+      notFound: 'food not found',
+    },
+  };
